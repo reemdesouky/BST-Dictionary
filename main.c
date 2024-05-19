f0775ebfd41f34ec0f5c64d35f2ff8cb98ba1bbd
reemdesouky <126808249+reemdesouky@users.noreply.github.com>
2024-05-19 19:51:22 +0300

    #

--- main.c
+++ main.c
@@ -2,24 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 typedef struct
-{
-    char data;
-    struct node *left,*right;
+{    char data[100];
+    struct node* left;
+    struct node* right;
 } node;
-node*newnode(char key)
+node* newnode(char* key)
 {
-    node *n=malloc(sizeof(node));
-    n->data=key;
+    node* n = malloc(sizeof(node));
+    strcpy(n->data, key);
+    n->left = NULL;
+    n->right = NULL;
     return n;
 }
-node* insert(node*root,char key)
+node* insert(node* root, char* key)
 {
-    if(root==NULL)
-        root=newnode(key);
-    else if(key<root->data)
-        root->left=insert(root->left,key);
-    else if(key>root->data)
-        root->right=insert(root->right,key);
+    if (root == NULL)
+        return newnode(key);
+    int cmp = strcmp(key, root->data);
+    if (cmp < 0)
+        root->left = insert(root->left, key);
+    else if (cmp > 0)
+        root->right = insert(root->right, key);
     return root;
 }
 void inorder(node*root)
@@ -27,15 +30,14 @@ void inorder(node*root)
     if(root!=NULL)
     {
         inorder(root->left);
-        printf("%c",root->data);
+        printf("%s",root->data);
         inorder(root->right);
     }
 }
 int main()
 {
-    node*n=newnode('c');
-    insert(n,'f');
-    printf("%c",n->data);
+    node* n = newnode("c");
+    insert(n, "f");
     inorder(n);
     return 0;
 }
